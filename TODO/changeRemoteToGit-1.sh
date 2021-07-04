ec050de552119371abe142b886f6fc69da7ccb97
Your Name <you@example.com>
2021-07-04 17:53:42 +0200

    .....

---
+++ TODO/changeRemoteToGit-1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+git remote set-url --push origin git@github.com:braindef/HackersCardgame23.git
+
+
+
+
+die letzten 5h aufgezeichnet, adaptiert und jetzt mensplaint urs blum odre siene sprachsynthese wieder meine schritte voraus was alles unkosher macht und urs blum dann sagen könnte dass es seine errungenschaft sei, also urs blum stielt meine veraltnesmuster, baut diese nach
+
+urs blum hat damasl über die alles nachbauenden chinesen gemotzt, das war somit eine schutzbehautpung?
+
+urs baut sich sein weltbild rund um mich auf und macht darin permaenet ein blutbat mit mir, weshalb?
+
+er vertickt dann das allenfalls an melanies kinder, die so von urs blum mit blutgeld bzw. immateriellen gütern vergiftet werden wo ich 1000e male fast drauf geganen bin und danach wird er die kinder von melanie damit erpressen "erbsünde", bzw. urs blum der 
+
+urs will sich das ansehen der kinder von melanie erschleichen und leakt, dass eines der kinder von melanie linux interessiert ist?
+
+melanie wird langsam älter, urs blum will frischfleisch?
+
+urs blum schaltet jetzt in den schutzbehauptugns modus, als weiss urs blum dass er schuldig ist und dann an claudine und melanie, allenfalls mal fragen in diese rechtung stellen zum schutz jüngere...
+
+
+
