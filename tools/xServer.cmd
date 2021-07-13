b5fa3bd6d1273cb8b8fa0f31d9baba46fca5d8b5
Your Name <you@example.com>
2021-07-13 05:36:52 +0200

    ...

--- tools/xServer.cmd
+++ tools/xServer.cmd
@@ -4,4 +4,15 @@ C:\Users\marc\PsTools\pskill.exe vcxsrv
 
 taskkill /IM VcXsrv.exe /T /f
 
-C:\Program Files\VcXsrv\vcxsrv.exe -multiwindow -clipboard -wgl -ac
+start cmd.exe /c "C:\Program Files\VcXsrv\vcxsrv.exe" -multiwindow -clipboard -wgl -ac
+
+
+REM wsl -d Ubuntu-18.04 -u marc -e "gnome-terminal &"
+
+REM export DISPLAY=192.168.1.233:0
+
+
+start cmd.exe /c wsl -d Ubuntu-18.04 -u marc -e "DISPLAY=192.168.1.233 gnome-terminal"
+start cmd.exe /c wsl -d Ubuntu-18.04 -u marc
+
+REM export DISPLAY="$(ipconfig.exe  |grep 192 |head -n1|cut -d: -f2 |cut -c 2-|tr -d '\r')"":0"
